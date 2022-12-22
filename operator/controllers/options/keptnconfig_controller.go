7cbe7769f1f2f200d892265effba0fa7a29279fb
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-12-22 10:27:51 +0100

    add new config field and fetch config in controller
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- operator/controllers/options/keptnconfig_controller.go
+++ operator/controllers/options/keptnconfig_controller.go
@@ -18,13 +18,17 @@ package options
 
 import (
 	"context"
+	"fmt"
 
+	controllererrors "github.com/keptn/lifecycle-toolkit/operator/controllers/errors"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	optionsv1alpha1 "github.com/keptn/lifecycle-toolkit/operator/apis/options/v1alpha1"
+	kltoptionsv1alpha1 "github.com/keptn/lifecycle-toolkit/operator/apis/options/v1alpha1"
 )
 
 // KeptnConfigReconciler reconciles a KeptnConfig object
@@ -39,17 +43,17 @@ type KeptnConfigReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the KeptnConfig object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *KeptnConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	config := &kltoptionsv1alpha1.KeptnConfig{}
+	err := r.Get(ctx, req.NamespacedName, config)
+	if errors.IsNotFound(err) {
+		return reconcile.Result{}, nil
+	}
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf(controllererrors.ErrCannotRetrieveConfigMsg, err)
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -57,6 +61,6 @@ func (r *KeptnConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 // SetupWithManager sets up the controller with the Manager.
 func (r *KeptnConfigReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&optionsv1alpha1.KeptnConfig{}).
+		For(&kltoptionsv1alpha1.KeptnConfig{}).
 		Complete(r)
 }
